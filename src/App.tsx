44c409cc4be770e9bc2d797fd4f621ffbf624c28
Muharrem Kocadere <mkocadere@gmail.com>
2021-11-24 21:50:53 +0300

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "antd/dist/antd.css";
+import "styles/global.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import Home from "pages";
+
+function App(): JSX.Element {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Home />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
