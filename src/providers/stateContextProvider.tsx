981e776ad96c5667e34bbbf3f6b2ac3302bbb5be
Muharrem Kocadere <mkocadere@gmail.com>
2021-12-02 02:06:48 +0300

    refactor types

--- src/providers/stateContextProvider.tsx
+++ src/providers/stateContextProvider.tsx
@@ -1,21 +1,34 @@
 import { useState } from "react";
 import { createContext } from "use-context-selector";
 
+type ReactSetStateType<T> = React.Dispatch<React.SetStateAction<T>>;
+
 export type State = {
     organization: string;
-    setOrganization: React.Dispatch<React.SetStateAction<string>>;
+    setOrganization: ReactSetStateType<State["organization"]>;
+    // organizationSearch: string;
+    // setOrganizationSearch: ReactSetStateType<State["organizationSearch"]>;
     repository: string;
-    setRepository: React.Dispatch<React.SetStateAction<string>>;
+    setRepository: ReactSetStateType<State["repository"]>;
 };
 
 export const stateContext = createContext<State>(null);
 
 export const StateProvider: React.FC = ({ children }) => {
     const [organization, setOrganization] = useState("");
+    // const [organizationSearch, setOrganizationSearch] = useState("");
     const [repository, setRepository] = useState("");
+
     return (
         <stateContext.Provider
-            value={{ organization, setOrganization, repository, setRepository }}
+            value={{
+                organization,
+                setOrganization,
+                // organizationSearch,
+                // setOrganizationSearch,
+                repository,
+                setRepository,
+            }}
         >
             {children}
         </stateContext.Provider>
