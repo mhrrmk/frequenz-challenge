3448878022d099599e8b63503427ef221f402c92
mhrrmk <mkocadere@gmail.com>
2023-06-10 16:27:41 +0200

    fix usedebounce

--- src/components/repositoryInput/index.tsx
+++ src/components/repositoryInput/index.tsx
@@ -26,7 +26,6 @@ export const RepositoryInput: React.FC<InputProps> = (props) => {
     (repository: string) => {
       setRepository(repository);
     },
-    undefined,
     [organization, repository],
   );
 
--- src/hooks/util/useDebounced.ts
+++ src/hooks/util/useDebounced.ts
@@ -4,8 +4,8 @@ import debounce from "lodash.debounce";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const useDebounced = <T extends (...args: any) => any>(
   func: T,
-  wait?: number,
   deps?: React.DependencyList,
+  wait?: number,
 ) => {
   return useMemo(() => debounce(func, wait ?? 500), deps ?? []);
 };
